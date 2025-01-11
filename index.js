f12993cefb1266544d614f291f4698461133dc8e
veekshith <veekshithgandhi@gmail.com>
2025-01-11 21:55:00 +0530

    installation meta api

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-require('dotenv').config();
-const { App } = require('@slack/bolt');
+require("dotenv").config();
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const moment = require('moment');
+const port = 3000;
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+async function getAdAccountInsights() {
+    const url = `https://graph.facebook.com/v21.0/${process.env.ACCOUNT_ID}/insights`;
+
+    try {
+        const response = await axios.get(url, {
+            params: {
+                fields: 'campaign_id,impressions,clicks,spend,reach,cpc,cpm,ctr,frequency',
+                time_range: '{"since":"2023-12-01","until":"2024-12-31"}',
+                level: 'campaign',
+                access_token: process.env.ACCESS_TOKEN
+            }
+        });
+
+        return response.data; 
+    } catch (error) {
+        throw new Error(error.response?.data || error.message);
+    }
+}
+
+
+app.get('/ad-insights', async (req, res) => {
+    try {
+        const insights = await getAdAccountInsights();
+        res.json(insights);
+    } catch (error) {
+        console.error('Error fetching insights:', error.message);
+        res.status(500).json({ error: error.message }); 
+    }
 });
 
-app.message('hello', async ({ message, say }) => {
-    console.log("MESSAGE ",message);
-    await say(`Hey there <@${message.user}>!`);
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
-
-(async () => {
-  await app.start(process.env.PORT || 3000);
-  console.log('Slack bot is running!');
-})();
